99f17dc76c2af95dc0b9ece637e495a50c5ee0a2
Cynthiachelangat <cchela789@gmail.com>
2024-07-03 13:57:31 +0300

    Login Manager for the user

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from website.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 Db_name = "database.db"
@@ -9,21 +10,29 @@ Db_name = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{Db_name}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{Db_name}'
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix= '/')
-    app.register_blueprint(auth, url_prefix= '/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
 
-     # Ensure the database is created when the app starts
+    # Ensure the database is created when the app starts
     with app.app_context():
         create_database()
     
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     return app
 
 def create_database():
